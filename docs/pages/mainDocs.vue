b0343adbd8c5ea6891c6f025bb0b81765c0783bf
gongph <gongph@foxmail.com>
2017-01-13 14:38:55 +0800

    new add docs components in pages

---
+++ docs/pages/mainDocs.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<ul class="demo-sidebar hide-on-tablets">
+	      <li><a href="#fui-button">Buttons</a></li>
+	      <li><a href="#fui-checkbox">Checkboxes</a></li>
+	      <li><a href="#fui-radio">Radio buttons</a></li>
+	      <li><a href="#fui-dropdown">Dropdown</a></li>
+	      <li><a href="#fui-input">Input</a></li>
+	      <li><a href="#fui-navbar">Navbar</a></li>
+	      <li><a href="#fui-pager">Pager</a></li>
+	      <li><a href="#fui-pagination">Pagination</a></li>
+	      <li><a href="#fui-progress">Progress</a></li>
+	      <li><a href="#fui-select">Select</a></li>
+	      <li><a href="#fui-switch">Switch</a></li>
+	      <li><a href="#fui-tagsinput">Tags Input</a></li>
+	      <li><a href="#fui-tooltip">Tooltip</a></li>
+	      <li><a href="#fui-slider">Slider</a></li>
+	      <li><a href="#fui-typeahead">Typeahead</a></li>
+	      <li><a href="#fui-videoplayer">Video Player</a></li>
+	    </ul>
+
+	    <h1 class="demo-headline">Vue-FlatUI</h1>
+
+	    <div class="container">
+	    	<slot></slot>
+	    </div>
+	</div>
+</template>
