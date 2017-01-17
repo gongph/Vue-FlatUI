3dcbfe2581caa66080da19bb2cb9192bccbabcc4
gongph <gongph@foxmail.com>
2017-01-17 16:09:15 +0800

    new add form-input component

---
+++ docs/pages/checkboxDocs.vue
@@ -0,0 +1,68 @@
+<template>
+	<div>
+	<div id="fui-checkbox"></div>
+	    	<div class="demo-row">
+	    		<div class="demo-title">
+	    			<h3>Checkboxes</h3>
+	    		</div>
+	    		<div class="demo-content">
+	    			<p>
+		            Using flat style checkboxes requires a JS plugin which creates all the custom markup for us. Let's take a look:
+		            </p>
+
+		            <div class="row">
+		            	<div class="col-md-6">
+		            		<p class="mb1">
+			    				<v-form-input type="checkbox" id="checkbox1" label @change.radiocheck="onRadioCheckChange">Checkbox</v-form-input>
+			    				<v-form-input type="checkbox" id="checkbox2" label checked>Checkbox</v-form-input>
+			    				<v-form-input type="checkbox" id="checkbox3" label checked disabled>Checkbox</v-form-input>
+			    				<v-form-input type="checkbox" id="checkbox4" label disabled>Checkbox</v-form-input>
+			    			</p>
+		            	</div>
+		            </div>
+
+		            <h6 class="ptl">
+		            	The JS:
+		            </h6>
+
+<pre class="prettyprint">&lt;v-form-input type="checkbox" id="checkbox1" label 
+	@change.radiocheck="onRadioCheckChange"&gt;Checkbox&lt;/v-form-input&gt;
+
+&lt;v-form-input type="checkbox" id="checkbox2" label 
+	checked&gt;Checkbox&lt;/v-form-input&gt;
+
+&lt;v-form-input type="checkbox" id="checkbox3" label 
+	checked disabled&gt;Checkbox&lt;/v-form-input&gt;
+
+&lt;v-form-input type="checkbox" id="checkbox4" label 
+	disabled&gt;Checkbox&lt;/v-form-input&gt;
+</pre>
+					<div class="pvm">
+						<h6>Events:</h6>
+					</div>
+					
+					<p class="mbl">
+						<strong>@change.radiocheck</strong><br/>
+						Fired when the checkbox state changes between
+						<span class="text-info">checked</span> and 
+						<span class="text-info">unchecked</span>
+					</p>
+					<p class="mbl">
+						<strong>@destroyed.radiocheck</strong><br/>
+						Fired when radiocheck destroys itself.
+					</p>
+
+	    		</div>
+	    	</div>
+	    </div>
+</template>
+
+<script>
+	export default {
+		methods: {
+			onRadioCheckChange (event) {
+				alert('radiocheck change ...')
+			}
+		}
+	}
+</script>
---
+++ src/components/form-input.vue
@@ -0,0 +1,129 @@
+<script>
+	export default {
+		render: function (c) {
+			var self = this, inputEl, formGroupEl, labelEl;
+			var attrs = {
+				type: self.type,
+				name: self.name,
+				placeholder: self.placeholder,
+				id: self.id,
+				value: self.value,
+				autocomplete: self.autocomplete,
+				autofocus: self.autofocus,
+				checked: self.checked,
+				disabled: self.disabled,
+				max: self.max,
+				min: self.min,
+				maxlength: self.maxlength,
+				minlength: self.minlength,
+				step: self.step,
+				multiple: self.multiple,
+				readonly: self.readonly,
+				required: self.required,
+				style: self.style
+			}
+			const on = {
+				focus: self.onFocus,
+				blur: self.onBlur,
+				change: self.onChange,
+				click: self.onClick,
+				input: self.onInput
+			}
+
+			// input type check
+			if (self.type === 'select' || self.type === 'textarea') {
+				if (self.type === 'select') {
+					inputEl = c('select', { attrs, on }, self.$slots.default)
+				} else {
+					var textareaChildren = self.$slots.default
+					if (self.value) {
+						delete attrs.value
+						textareaChildren = self.value
+					}
+					inputEl = c('textarea', { attrs, on }, textareaChildren)
+				}
+			} else {
+				if (self.$slots.default && self.$slots.default.lengthh > 0 || !self.type) {
+					inputEl = self.$slots.default
+				} else {
+					inputEl = c('input', { attrs, on })
+				}
+			}
+
+			// checkbox or radio button
+			if (self.label) {
+				labelEl = c('label', {
+					class: {
+						[self.type]: (self.type === 'checkbox' || self.type === 'radio') ? true : false
+					},
+					attrs: self.id ? { for: self.id } : {},
+					on: {
+						'change.radiocheck': self.onRadiocheckChange,
+						'destroyed.radiocheck': self.onRadiocheckDestroyed
+					}
+				}, [inputEl, self.$slots.default]);
+
+			} 
+
+			// div.form-group
+			if (self.group && self.label) {
+				formGroupEl = c('div', {
+					staticClass: 'form-group',
+				}, [labelEl]);
+			}
+
+			if (!labelEl && !formGroupEl) return inputEl
+			if (labelEl && !formGroupEl) return labelEl
+			if (formGroupEl) return formGroupEl
+		},
+		props: {
+			// Inputs
+			type: String,
+			name: String,
+			placeholder: String,
+			id: String,
+			value: [String, Number],
+			autocomplete: String,
+			autofocus: String,
+			checked: Boolean,
+			disabled: Boolean,
+			max: [String, Number],
+			min: [String, Number],
+			maxlength: [String, Number],
+			minlength: [String, Number],
+			step: [String, Number],
+			multiple: Boolean,
+			readonly: Boolean,
+			required: Boolean,
+			style: String,
+
+			label: Boolean,
+			group: Boolean
+		},
+		mounted () {
+			const self = this 
+			if (self.type === 'checkbox' || self.type === 'radio') {
+				$(':' + self.type).radiocheck()
+			}
+		},
+		methods: (function(){
+			const eventMethods = {
+				onInput (event) {
+					this.$emit('input', event.target.value)
+				},
+				onRadiocheckChange (event) {
+					this.$emit('change.radiocheck', event)
+				},
+				onRadiocheckDestroyed (event) {
+					this.$emit('destroyed.radiocheck', event)
+				}
+			}
+			'Focus Blur Change Click'.split(' ').forEach((ev) => {
+				eventMethods['on' + ev] = function (event) {
+					this.$emit(ev.toLowerCase(), event)
+				}
+			})
+			return eventMethods
+		})()
+	}
+</script>
